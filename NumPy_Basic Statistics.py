bc1c6c410e072e7f64890a406fe26a42723b5d81
Kunal Rathod <36470138+KunalRRathod@users.noreply.github.com>
2018-08-29 19:42:06 +0530

    Update NumPy_Basic Statistics.py

--- NumPy_Basic Statistics.py
+++ NumPy_Basic Statistics.py
@@ -1,5 +1,6 @@
 import pandas as pd
-np_baseball = pd.read_csv("http://s3.amazonaws.com/assets.datacamp.com/course/intro_to_python/baseball.csv")[['Height', 'Weight', 'Age']].as_matrix()
+# any CSV here
+np_baseball = pd.read_csv(".csv")[['Height', 'Weight', 'Age']].as_matrix()
 np_baseball[slice(0, 1015, 50), 0] = np_baseball[slice(0, 1015, 50), 0]*1000
 
 # Import numpy
