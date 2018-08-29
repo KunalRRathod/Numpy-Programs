3014a1a3aa68c96b7af64d36dd396f34a79e514a
Kunal Rathod <36470138+KunalRRathod@users.noreply.github.com>
2018-08-29 19:37:48 +0530

    Update Baseball data in 2D form.py

--- Baseball data in 2D form.py
+++ Baseball data in 2D form.py
@@ -1,5 +1,6 @@
 import pandas as pd
-baseball = pd.read_csv("http://s3.amazonaws.com/assets.datacamp.com/course/intro_to_python/baseball.csv")[['Height', 'Weight']].as_matrix().tolist()
+#Use any CSV file here
+baseball = pd.read_csv(".csv")[['Height', 'Weight']].as_matrix().tolist()
 # Import numpy package
 import numpy as np
 
